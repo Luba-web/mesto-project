e0664fa7d7bf13008da8920aea86d5b0edbddaf3
Lyubov Vasileva <LDVasileva87@yandex.ru>
2022-01-28 12:25:44 +0300

    описание функция и добавление карточек

--- index.js
+++ index.js
@@ -1,57 +1,154 @@
-//находим переменные для модального окна
+//находим переменные
 const popupProfile = document.querySelector("#profile");
-const btnProfileClose = document.querySelector("#profileClose");
-const btnPen = document.querySelector(".profile__button-pen");
-const btnProfileSave = document.querySelector("#profileSave");
+const popupCardMesto = document.querySelector("#cardMesto");
+const popupImages = document.querySelector("#popupImages");
 
-//функция открытия и закрытие модального окна
-function popupOpen(popup) {
+const profileEditForm = document.forms["profileForm"];
+const nameInput = profileEditForm.elements.firstName;
+const jobInput = profileEditForm.elements.profession;
+
+const cardEditMestoForm = document.forms["cardMestoForm"];
+const nameInputCard = cardEditMestoForm.elements.nameImg;
+const linkInputCard = cardEditMestoForm.elements.linkImg;
+
+const btnPen = document.querySelector(".profile__button-pen");
+const btnProfileSave = document.querySelector("#profileForm");
+const closePopupBtns = document.querySelectorAll(".popup__button-close");
+
+const profileName = document.querySelector(".profile__title");
+const profileJob = document.querySelector(".profile__subtitle");
+
+const btnCardMestoAdd = document.querySelector("#profileAdd");
+const btnCardMestoSave = document.querySelector("#cardMestoForm");
+
+const cardContainer = document.querySelector(".cards");
+const cardTemlate = document.querySelector("#cardTemplate").content;
+
+const popupPhoto = document.querySelector("#photoMesto");
+const initialCards = [
+  {
+    name: "Мася",
+    link: "./images/1.jpg",
+  },
+  {
+    name: "Суся",
+    link: "./images/2.jpg",
+  },
+  {
+    name: "Вася",
+    link: "./images/3.jpg",
+  },
+  {
+    name: "Ляся",
+    link: "./images/4.jpg",
+  },
+  {
+    name: "Ося",
+    link: "./images/5.jpg",
+  },
+  {
+    name: "Мистер Бося Старший",
+    link: "./images/6.jpg",
+  },
+];
+
+//функция открытие модального окна
+function openPopup(popup) {
   popup.classList.add("popup_opened");
 }
-function popupClose(popup) {
-  popup.classList.remove("popup_opened");
+
+//функция закрытие модального окна
+function closePopup() {
+  const openPopup = document.querySelector(".popup_opened");
+  openPopup.classList.remove("popup_opened");
 }
 
-// вызов метода открытия и закрытия модального окна popupProfile
-btnPen.addEventListener("click", () => {
-  popupOpen(popupProfile);
-});
-btnProfileClose.addEventListener("click", () => {
-  popupClose(popupProfile);
-});
-
-// Находим форму в DOM
-const formElement = document.querySelector(".Form");
-
-function handleFormSubmit(event) {
-  event.preventDefault()
-  let nameInput = formElement.querySelector(".form__input_name_first-name");
-  let jobInput = formElement.querySelector(".form__input_name_profession");
-  let profileName = document.querySelector(".profile__title");
-  let profileJob = document.querySelector(".profile__subtitle");
+//функция 'Submit profileForm'
+function formSubmitProfile(event) {
+  event.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
+  closePopup();
 }
 
-// кнопка сохранения popupProfile
-btnProfileSave.addEventListener('click', () => {
-  formElement.addEventListener('submit', handleFormSubmit);
-  popupClose(popupProfile);
+//функция 'Submit cardMestoForm'
+function formSubmitCard(event) {
+  event.preventDefault();
+  const data = {};
+  data.name = nameInputCard.value;
+  data.link = linkInputCard.value;
+  addCard(data);
+  closePopup();
+  nameInputCard.value = "";
+  linkInputCard.value = "";
+}
+
+//функция добавление карточек
+function addCard(cardNew) {
+  const cardElement = renderCard(cardNew); //вопрос
+  cardContainer.prepend(cardElement);
+}
+
+//функция удаления карточки
+function deleteCard(event) {
+  console.log(event);
+  const card = event.target.closest(".cards__item");
+  card.remove();
+}
+
+//функция добавления карточек пользователем
+function renderCard(cardNew) {
+  const cardElement = cardTemlate.querySelector(".cards__item").cloneNode(true);
+  const imageCard = cardElement.querySelector(".cards__images");
+  const nameCard = cardElement.querySelector(".cards__text");
+  nameCard.textContent = cardNew.name;
+  imageCard.src = cardNew.link;
+  imageCard.alt = cardNew.name;
+  cardElement
+    .querySelector(".cards__like")
+    .addEventListener("click", likeActive);
+  cardElement
+    .querySelector(".cards__button-delete")
+    .addEventListener("click", deleteCard);
+  imageCard.addEventListener("click", () => openPhoto(cardNew));
+  return cardElement;
+}
+
+//функция открытия картинки/фото
+const openPhoto = (cardNew) => {
+  popupImages.querySelector(".popup__images").src = cardNew.link;
+  popupImages.querySelector(".popup__images").alt = cardNew.name;
+  popupImages.querySelector(".popup__caption").textContent = cardNew.name;
+  openPopup(popupImages);
+};
+
+//функция клик лайка
+function likeActive(event) {
+  event.target.classList.toggle("cards__like_active");
+}
+
+//обрабочики событий(слушатели)
+
+//слушатель для всех кнопок закрытия
+closePopupBtns.forEach((item) => {
+  item.addEventListener("click", closePopup);
 });
 
+// кнопки сохранения popupProfile и CardMesto
+btnProfileSave.addEventListener("submit", formSubmitProfile);
+btnCardMestoSave.addEventListener("submit", formSubmitCard);
 
-// контейнер с картами
-const popupCardMesto = document.querySelector("#cardMesto");
-const btnCardMestoClose = document.querySelector("#cardMestoClose");
-const btnCardMestoAdd = document.querySelector("#profileAdd");
-const btnCardMestoSave = document.querySelector("#cardMestoSave");
+//добовляем слушатели для модального окна popupProfile
+btnPen.addEventListener("click", () => {
+  openPopup(popupProfile);
+});
 
+//добовляем слушатели для модального окна popupCardMesto
 btnCardMestoAdd.addEventListener("click", () => {
-  popupOpen(popupCardMesto);
-}, false);
-btnProfileClose.addEventListener("click", () => {
-  popupClose(popupCardMesto);
+  openPopup(popupCardMesto);
 });
-btnCardMestoSave.addEventListener('click', () => {
-  popupClose(popupCardMesto);
-});
+
+//добовление карточек
+for (let i = 0; i < initialCards.length; i++) {
+  addCard(initialCards[i]);
+}
